8be6fb3dbe52b9cc4cd947ada035e3c8986d8f04
KicamonIce <1532931259@qq.com>
2023-03-03 14:06:28 +0800

    2023/03/03,14:06

--- test.cpp
+++ test.cpp
@@ -16,7 +16,6 @@
 */
 //#pragma GCC optimize(2)
 #include <bits/stdc++.h>
-#include <valarray>
 using namespace std;
 #define endl '\n'
 #define all(a) a.begin(), a.end()
@@ -26,88 +25,29 @@ using namespace std;
 #define pb push_back
 #define Debug(x) cout << #x << ':' << x << endl
 int input = 1;
-const int mod = 1e9 + 7, N = 2e5 + 10;
-
-int e[M], ne[M], w[M], h[N], idx;
-
-void add(int a, int b, int c)
-{
-    e[idx] = b, ne[idx] = h[a], w[idx] = c, h[a] = idx++;
-}
-
-bool vis[N];
-int dist[N], cnt[N];
-bool spfa()
-{
-    queue<int> q;
-    for (int i = 1; i <= n; ++i)
-    {
-        q.push(i);
-        vis[i] = true;
-    }
-    while (!q.empty())
-    {
-        int t = q.front();
-        q.pop();
-        vis[t] = false;
-        for (int i = h[t]; ~i; i = ne[i])
-        {
-            int j = e[i];
-            if (dist[j] > dist[t] + w[i])
-            {
-                dist[j] = dist[t] + w[i];
-                cnt[j] = cnt[t] + 1;
-                if (cnt[j] >= n)
-                    return true;
-                if (!vis[j])
-                {
-                    q.push(j);
-                    vis[j] = true;
-                }
-            }
-        }
-    }
-
-    return false;
-}
 
 void solve()
 {
     int n, m;
     cin >> n >> m;
-    for (int i = 1; i <= n; ++i)
-        cin >> a[i];
-    st_size = n;
-    build();
-    while (m--)
-    {
-        int op, l, r;
-        cin >> op >> l >> r;
-        if (op == 1)
+    string s, k;
+    cin >> s >> k;
+    vint num(26), sum(26);
+    for (int i = 0; i < n; ++i)
+        num[s[i] - 'a']++, sum[k[i] - 'a']++;
+    for (int i = 0; i < 26; ++i)
+        if (num[i] != sum[i])
         {
-            int d;
-            cin >> d;
-            Debug(op);
-            //	modify(1,l,r,1,n,(tag){d,1});
+            cout << "NO\n";
+            return;
         }
-        else if (op == 2)
+    for (int i = 0; i < n; ++i)
+        if (s[i] != k[i] && max(i, n - i - 1) < m)
         {
-            int d;
-            cin >> d;
-            Debug(op);
-            //	modify(1,l,r,1,n,(tag){0,d});
+            cout << "NO\n";
+            return;
         }
-        else if (op == 3)
-        {
-            int d;
-            cin >> d;
-            Debug(op);
-            //	modify(1,l,r,1,n,(tag){d,0});
-        }
-        else
-            Debug(op);
-        //	cout << query(1,l,r) << endl;
-    }
+    cout << "YES\n";
 }
 
 signed main()
