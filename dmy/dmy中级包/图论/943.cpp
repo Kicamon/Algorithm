6a6278ba44bd031d051c11024636c460889ea795
KicamonIce <1532931259@qq.com>
2023-04-01 16:41:22 +0800

    2023/04/01,16:41

---
+++ dmy/dmy中级包/图论/943.cpp
@@ -0,0 +1,104 @@
+/*
+[[ ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷ ]],
+[[ ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇ ]],
+[[ ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽ ]],
+[[ ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕ ]],
+[[ ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕ ]],
+[[ ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕ ]],
+[[ ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄ ]],
+[[ ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕ ]],
+[[ ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿ ]],
+[[ ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ]],
+[[ ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟ ]],
+[[ ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠ ]],
+[[ ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙ ]],
+[[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
+*/
+// #pragma GCC optimize(2)
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define ll long long
+#define vint vector<int>
+#define pb push_back
+#define Debug(x) cout << #x << ':' << x << endl
+const int N = 1e5 + 10;
+
+int n, m;
+vector<int> e[N];
+int dfn[N], low[N], bel[N], idx, cnt;
+/*  是否被遍历过，是否能跳出，属于哪个强连通分量 */
+bool ins[N]; /* 是否作为强连通分量被弹出 */
+stack<int> stk;
+vector<vector<int>> scc;
+
+void dfs(int u)
+{
+    dfn[u] = low[u] = ++idx;
+    ins[u] = true;
+    stk.push(u);
+    for (auto v : e[u])
+    {
+        if (!dfn[v])
+            dfs(v);
+        if (ins[v])
+            low[u] = min(low[u], low[v]);
+    }
+    if (dfn[u] == low[u])
+    {
+        vector<int> c;
+        ++cnt;
+        while (true)
+        {
+            int v = stk.top();
+            c.push_back(v);
+            ins[v] = false;
+            bel[v] = cnt;
+            stk.pop();
+            if (v == u)
+                break;
+        }
+        sort(c.begin(), c.end());
+        scc.push_back(c);
+    }
+}
+
+void tarjan()
+{
+    cin >> n >> m;
+    for (int i = 0; i < m; ++i)
+    {
+        int a, b;
+        cin >> a >> b;
+        e[a].push_back(b);
+    }
+
+    for (int i = 1; i <= n; ++i)
+        if (!dfn[i])
+            dfs(i);
+    sort(scc.begin(), scc.end());
+    for (auto c : scc)
+    {
+        for (auto x : c)
+            cout << x << ' ';
+        cout << endl;
+    }
+}
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    // clock_t start, finish;
+    // start = clock();
+
+    tarjan();
+
+    // finish = clock();
+    // cout <<endl<<"the time cost is:" << double(finish - start) / CLOCKS_PER_SEC<<endl;
+
+    return 0;
+}
