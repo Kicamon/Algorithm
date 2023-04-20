9b92fd7ca332d7ea58f0e14b81a9a4b9b90407e1
KicamonIce <1532931259@qq.com>
2023-04-20 20:50:51 +0800

    2023/04/20,20:50

--- other/pta/L2-002.cpp
+++ other/pta/L2-002.cpp
@@ -24,63 +24,58 @@ using namespace std;
 #define vint vector<int>
 #define pb push_back
 #define Debug(x) cout << #x << ':' << x << endl
-const int N = 1e5 + 10;
-
-struct node
-{
-    string a;
-    int b;
-    string c;
-    bool operator<(const node &t) const
-    {
-        return a < t.a;
-    }
-};
+const int N = 1e4 + 10;
 
 bool vis[N];
 
 signed main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-
     // clock_t start, finish;
     // start = clock();
 
     string n;
     int m;
     cin >> n >> m;
-    vector<node> v(m);
+    vector<array<int, 3>> v(m);
+    int root;
+    set<int> sv;
     for (int i = 0; i < m; ++i)
-        cin >> v[i].a >> v[i].b >> v[i].c;
+    {
+        cin >> v[i][0] >> v[i][1] >> v[i][2];
+        if (!sv.count(v[i][0]))
+            root = v[i][0];
+        sv.insert(v[i][2]);
+    }
     sort(all(v));
-    vector<node> um, hm;
-    for (int i = 0; i < m; ++i)
+    vector<array<int, 3>> um, hm;
+    while (root != -1)
     {
         int l = 0, r = m - 1;
         while (l < r)
         {
             int mid = l + r >> 1;
-            if (v[mid].a < n)
+            if (v[mid][0] < root)
                 l = mid + 1;
             else
                 r = mid;
         }
-        n = v[l].c;
-        int k = abs(v[l].b);
+        int k = abs(v[l][1]);
         if (vis[k])
         {
-            um.back().c = v[l].c;
+            if (!hm.empty())
+                hm.back()[2] = v[l][0];
+            um.back()[2] = v[l][2];
             hm.push_back(v[l]);
         }
         else
             um.push_back(v[l]);
         vis[k] = true;
+        root = v[l][2];
     }
     for (auto x : um)
-        cout << x.a << ' ' << x.b << ' ' << x.c << endl;
-    for (int i = 0; i < (int)hm.size(); ++i)
-        cout << hm[i].a << ' ' << hm[i].b << ' ' << (i < (int)hm.size() - 1 ? hm[i + 1].a : hm[i].c) << endl;
+        printf("%05d %d %05d\n", x[0], x[1], x[2]);
+    for (auto x : hm)
+        printf("%05d %d %05d\n", x[0], x[1], x[2]);
 
     // finish = clock();
     // cout <<endl<<"the time cost is:" << double(finish - start) / CLOCKS_PER_SEC<<endl;
