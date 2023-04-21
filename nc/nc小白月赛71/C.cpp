b5594029b69277b4ad4fe60391dd31e11e2913d3
KicamonIce <1532931259@qq.com>
2023-04-21 21:45:50 +0800

    2023/04/21,21:45

---
+++ nc/nc小白月赛71/C.cpp
@@ -0,0 +1,87 @@
+/*
+[[ ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷ ]],
+[[ ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇ ]],
+[[ ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽ ]],
+[[ ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕ ]],
+[[ ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕ ]],
+[[ ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕ ]],
+[[ ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄ ]],
+[[ ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕ ]],
+[[ ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿ ]],
+[[ ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ]],
+[[ ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟ ]],
+[[ ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠ ]],
+[[ ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙ ]],
+[[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
+*/
+// #pragma GCC optimize(2)
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define ll long long
+#define vint vector<int>
+#define pb push_back
+#define Debug(x) cout << #x << ':' << x << endl
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    // clock_t start, finish;
+    // start = clock();
+
+    ll p, q;
+    cin >> p >> q;
+    ll cnt = p;
+    ll y = p;
+    bool f = false;
+    for (int i = 2; i <= q; ++i)
+    {
+        y *= cnt;
+        if (y < cnt || y > 1e18)
+        {
+            f = true;
+            break;
+        }
+    }
+    if (f)
+    {
+        cout << 2 << endl;
+        return 0;
+    }
+    ll res = pow(p, q);
+    int idx = 3;
+    while (true)
+    {
+        if (cnt >= 60)
+            break;
+        else
+        {
+            f = false;
+            ll k = cnt;
+            for (int i = 2; i <= res; ++i)
+            {
+                k *= cnt;
+                if (k < cnt || k > 1e18)
+                {
+                    f = true;
+                    break;
+                }
+            }
+            if (f)
+                break;
+            cnt = res;
+            res = k;
+        }
+        idx++;
+    }
+    cout << idx << endl;
+
+    // finish = clock();
+    // cout <<endl<<"the time cost is:" << double(finish - start) / CLOCKS_PER_SEC<<endl;
+
+    return 0;
+}
