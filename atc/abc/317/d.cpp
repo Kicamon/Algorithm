39e00fe2151fd5ef72db97bc1f71216b2ec6a2cb
KicamonIce <1532931259@qq.com>
2023-09-08 07:27:40 +0800

    2023/09/08,07:27

--- atc/abc/317/d.cpp
+++ atc/abc/317/d.cpp
@@ -20,7 +20,7 @@ using namespace std;
 #define endl '\n'
 #define ll long long
 #define Debug(x) cout << #x << ':' << x << endl
-#define int long long
+#define all(x) (x).begin(), (x).end()
 
 signed main() {
     ios::sync_with_stdio(false);
@@ -28,32 +28,18 @@ signed main() {
 
     int n;
     cin >> n;
-    vector<int> a(n);
+    vector<array<int, 3>> a(n);
+    vector<int> res(2);
+    vector<array<int, 2>> dp;
     for (int i = 0; i < n; ++i) {
-        cin >> a[i];
-    }
-    vector<array<int, 2>> dp(n);
-    vector<int> pre(n + 1, -1);
-    vector<int> ans(n);
-    pre[a[0]] = 0;
-    dp[0][1] = 1;
-    for (int i = 1; i < n; ++i) {
-        if (pre[a[i]] != -1) {
-            dp[i][0] = pre[a[i]];
-            dp[i][1] = dp[pre[a[i]]][1] + 1;
-            ans[i] = ans[dp[i][0]] + dp[dp[i][0]][1] * (i - dp[i][0] - 1);
+        cin >> a[i][0] >> a[i][1] >> a[i][2];
+        if (a[i][0] > a[i][1]) {
+            res[0] += a[i][2];
         } else {
-            dp[i][1] = 1;
+            res[1] += a[i][2];
+            dp.push_back({(a[i][1] + a[i][0] + 1) >> 1, a[i][2]});
         }
-        pre[a[i]] = i;
-        /* cout << ans[i] << ' '; */
     }
-    /* cout << endl; */
-    int res = 0;
-    for (int i = 0; i < n; ++i) {
-        res += ans[i];
-    }
-    cout << res << endl;
 
     return 0;
 }
--- cf/18+/72/e.cpp
+++ cf/18+/72/e.cpp
@@ -20,40 +20,55 @@ using namespace std;
 #define endl '\n'
 #define ll long long
 #define Debug(x) cout << #x << ':' << x << endl
-#define int long long
 
-signed main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-
+void solve() {
     int n;
     cin >> n;
     vector<int> a(n);
     for (int i = 0; i < n; ++i) {
         cin >> a[i];
     }
-    vector<array<int, 2>> dp(n);
-    vector<int> pre(n + 1, -1);
-    vector<int> ans(n);
-    pre[a[0]] = 0;
-    dp[0][1] = 1;
-    for (int i = 1; i < n; ++i) {
-        if (pre[a[i]] != -1) {
-            dp[i][0] = pre[a[i]];
-            dp[i][1] = dp[pre[a[i]]][1] + 1;
-            ans[i] = ans[dp[i][0]] + dp[dp[i][0]][1] * (i - dp[i][0] - 1);
-        } else {
-            dp[i][1] = 1;
-        }
-        pre[a[i]] = i;
-        /* cout << ans[i] << ' '; */
-    }
-    /* cout << endl; */
+    string s;
+    cin >> s;
+    vector<int> sum(n + 1);
     int res = 0;
     for (int i = 0; i < n; ++i) {
-        res += ans[i];
+        if (s[i] == '0') {
+            res ^= a[i];
+        }
+        sum[i + 1] = sum[i] ^ a[i];
     }
-    cout << res << endl;
+    int k;
+    cin >> k;
+    for (int _ = 0; _ < k; ++_) {
+        int op;
+        cin >> op;
+        if (op == 1) {
+            int l, r;
+            cin >> l >> r;
+            l--;
+            res ^= sum[l] ^ sum[r];
+        } else {
+            int g;
+            cin >> g;
+            if (g == 0) {
+                cout << res << ' ';
+            } else {
+                cout << (res ^ sum[n]) << ' ';
+            }
+        }
+    }
+    cout << endl;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
 
     return 0;
 }
