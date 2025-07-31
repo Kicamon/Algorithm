8f430432ef7311f5e85ca79d936707437d034fe5
Kicamon <kicamonice@gmail.com>
2025-07-31 18:28:15 +0800

    update

--- cf/21+/28/d.cc
+++ cf/21+/28/d.cc
@@ -27,9 +27,29 @@ void solve() {
         int n;
         cin >> n;
         vector<int> a(n);
+        vector<ll> dp(n);
         for (int &x : a) {
                 cin >> x;
         }
+        dp[0] = 1, dp[1] = a[1] > a[0] ? 4 : 3;
+        for (int i = 2; i < n; ++i) {
+                dp[i] = max(dp[i - 2] + i, dp[i - 1] + i);
+        }
+        // for (int i = 2; i < n; ++i) {
+        //         if (a[i] < a[i - 1]) {
+        //                 temp[0] = dp[i - 1] + i;
+        //         }
+        //         if (a[i] < a[i - 2]) {
+        //                 temp[1] = dp[i - 2] + i;
+        //         }
+        //
+        //         temp = { 0, 0 };
+        // }
+        ll temp = 0;
+        for (int i = 0; i < n; ++i) {
+                temp += dp[i];
+        }
+        cout << temp << endl;
 }
 
 signed main() {
