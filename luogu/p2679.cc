fa31c951cd46a0266de4af6efe517bbc8d11a856
Kicamon <kicamonice@gmail.com>
2025-08-05 01:20:43 +0800

    update

--- luogu/p2679.cc
+++ luogu/p2679.cc
@@ -23,7 +23,7 @@ using namespace std;
 #define all(x) (x).begin(), (x).end()
 const int mod = 1000000007;
 const int N = 1005, M = 205;
-int dp[N][M][M];
+ll dp[2][M][M][2];
 
 signed main() {
         ios::sync_with_stdio(false);
@@ -34,24 +34,28 @@ signed main() {
         string a, b;
         cin >> a >> b;
         a = ' ' + a, b = ' ' + b;
-        dp[1][1][0] = 1;
+        dp[0][0][0][0] = dp[1][0][0][0] = 1;
+        int val = 0;
         for (int i = 1; i <= n; ++i) {
+                val ^= 1;
                 for (int j = 1; j <= m; ++j) {
-                        for (int k = 0; k <= p; ++k) {
-                                if (i < n) {
-                                        (dp[i + 1][j][k] += dp[i][j][k]) %= mod;
-                                }
-                                for (int t = 1; i + t <= n && j + t <= m; ++t) {
-                                        if (a[i + t] == b[j + t]) {
-                                                (dp[i + t][j + t][k + 1] += dp[i][j][k]) %= mod;
-                                        } else {
-                                                break;
-                                        }
+                        for (int k = 1; k <= p; ++k) {
+                                if (a[i] == b[j]) {
+                                        dp[val][j][k][0] =
+                                                (dp[val ^ 1][j][k][0] + dp[val ^ 1][j][k][1]) % mod;
+                                        dp[val][j][k][1] = (dp[val ^ 1][j - 1][k][1] +
+                                                            dp[val ^ 1][j - 1][k - 1][1] +
+                                                            dp[val ^ 1][j - 1][k - 1][0]) %
+                                                           mod;
+                                } else {
+                                        dp[val][j][k][0] =
+                                                (dp[val ^ 1][j][k][0] + dp[val ^ 1][j][k][1]) % mod;
+                                        dp[val][j][k][1] = 0;
                                 }
                         }
                 }
         }
-        cout << dp[n][m][p] << endl;
+        cout << (dp[val][m][p][0] + dp[val][m][p][1]) % mod << endl;
 
         return 0;
 }
