86cb79083a2093aa0cc3627c93b8cde3105ebad9
Kicamon <kicamonice@gmail.com>
2025-08-05 18:16:32 +0800

    update

---
+++ luogu/p5020.cc
@@ -0,0 +1,60 @@
+/*
+[[ ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷ ]],
+[[ ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇ ]],
+[[ ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽ ]],
+[[ ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕ ]],
+[[ ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕ ]],
+[[ ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕ ]],
+[[ ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄ ]],
+[[ ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕ ]],
+[[ ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿ ]],
+[[ ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ]],
+[[ ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟ ]],
+[[ ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠ ]],
+[[ ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙ ]],
+[[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
+*/
+/* #pragma GCC optimize(2) */
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+#define endl '\n'
+#define ll long long
+#define Debug(x) cout << #x << ':' << x << endl
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+        int n;
+        cin >> n;
+        vector<int> a(n), b;
+        for (int &x : a) {
+                cin >> x;
+        }
+        sort(all(a));
+        int len = *max_element(all(a)), idx = 0;
+        vector<int> dp(len + 1);
+        dp[0] = 1;
+        for (int i = 0; i < n; ++i) {
+                for (int j = a[i]; j <= len; ++j) {
+                        dp[j] += dp[j - a[i]];
+                }
+                if (dp[a[i]] == 1) {
+                        idx++;
+                }
+        }
+        cout << idx << endl;
+}
+
+signed main() {
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+
+        int t;
+        cin >> t;
+        while (t--) {
+                solve();
+        }
+
+        return 0;
+}
---
+++ vj/b.cc
@@ -0,0 +1,110 @@
+/*
+[[ ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷ ]],
+[[ ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇ ]],
+[[ ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽ ]],
+[[ ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕ ]],
+[[ ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕ ]],
+[[ ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕ ]],
+[[ ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄ ]],
+[[ ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕ ]],
+[[ ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿ ]],
+[[ ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ]],
+[[ ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟ ]],
+[[ ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠ ]],
+[[ ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙ ]],
+[[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
+*/
+/* #pragma GCC optimize(2) */
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <vector>
+#include <array>
+using namespace std;
+#define endl '\n'
+#define ll long long
+#define Debug(x) cout << #x << ':' << x << endl
+#define all(x) (x).begin(), (x).end()
+
+array<bool, 31> gbl(int x) {
+        array<bool, 31> res;
+        fill(all(res), 0);
+        int idx = 0;
+        while (x) {
+                res[idx++] = x % 2;
+                x >>= 1;
+        }
+        return res;
+}
+
+signed main() {
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+
+        int n;
+        cin >> n;
+        vector<array<bool, 31> > a(n);
+        vector<int> x(n);
+        array<int, 32> vis;
+        fill(all(vis), 0);
+
+        function<void(bool, int)> update = [&](bool ud, int i) {
+                if (ud) {
+                        for (int j = 0; j < 32; ++j) {
+                                if (a[i][j]) {
+                                        vis[j]--;
+                                }
+                        }
+                } else {
+                        for (int j = 0; j < 32; ++j) {
+                                if (a[i][j]) {
+                                        vis[j]++;
+                                }
+                        }
+                }
+        };
+
+        for (int i = 0; i < n; ++i) {
+                cin >> x[i];
+                a[i] = gbl(x[i]);
+                update(0, i);
+        }
+
+        int m;
+        cin >> m;
+        int idx = -1;
+
+        function<void(int)> change = [&](int v) {
+                for (int i = 0; i < n; ++i) {
+                        if ((x[i] & (1 << v)) && (x[i] >= (1 << v))) {
+                                x[i] <<= (idx - i);
+                                update(1, i);
+                                a[i] = gbl(x[i]);
+                                update(0, i);
+                        }
+                }
+        };
+
+        for (int i = 30; ~i; --i) {
+                if (!vis[i]) {
+                        continue;
+                }
+                if (idx == -1) {
+                        idx = i;
+                        continue;
+                }
+                if ((idx - i) * vis[i] <= m) {
+                        m -= (idx - i) * vis[i];
+                        change(i);
+                } else {
+                        idx = i;
+                }
+        }
+        int ans = 0;
+        for (int s : x) {
+                ans |= s;
+        }
+        cout << ans << endl;
+
+        return 0;
+}
