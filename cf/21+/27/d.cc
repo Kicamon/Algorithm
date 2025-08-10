f84f7a20d9959e2ef9f573569bb53e23c37a7370
Kicamon <kicamonice@gmail.com>
2025-08-11 02:14:22 +0800

    update

--- cf/21+/27/d.cc
+++ cf/21+/27/d.cc
@@ -17,7 +17,6 @@
 /* #pragma GCC optimize(2) */
 #include <functional>
 #include <iostream>
-#include <numeric>
 #include <queue>
 #include <vector>
 using namespace std;
@@ -30,72 +29,26 @@ const int N = 2e5 + 10;
 
 ll fac[N];
 
-struct DSU {
-        vector<int> f, siz;
-
-        DSU(int n) {
-                f.resize(n);
-                iota(all(f), 0);
-                siz.assign(n, 1);
-        }
-
-        int find(int x) {
-                while (x != f[x]) {
-                        x = f[x] = f[f[x]];
-                }
-                return x;
-        }
-
-        bool merge(int x, int y) {
-                x = find(x);
-                y = find(y);
-                if (x == y) {
-                        return false;
-                }
-                f[y] = x;
-                siz[x] += siz[y];
-                return true;
-        }
-
-        bool same(int x, int y) {
-                return find(x) == find(y);
-        }
-
-        int size(int x) {
-                return siz[find(x)];
-        }
-};
-
 void solve() {
         int n, m;
         cin >> n >> m;
         vector<vector<int> > g(n + 1);
         vector<bool> vis(n + 1, 1);
-        DSU dsu(n + 1);
-        bool flag = false;
         for (int i = 0, a, b; i < m; ++i) {
                 cin >> a >> b;
-                if (!dsu.merge(a, b)) {
-                        flag = true;
-                }
                 g[a].push_back(b), g[b].push_back(a);
-        }
-
-        if (flag) {
-                cout << 0 << endl;
-                return;
         }
 
         function<ll(int)> get_num = [&](int root) {
                 ll ans = 1;
                 int tag = g[root].size() > 1, res = 0;
+                vis[root] = false;
 
                 queue<int> q;
                 q.push(root);
                 while (!q.empty()) {
                         int node = q.front();
                         q.pop();
-                        vis[node] = false;
                         tag++;
                         for (int s : g[node]) {
                                 if (g[s].size() > 1) {
@@ -104,22 +57,24 @@ void solve() {
                                         }
                                 } else {
                                         res++;
-                                        vis[s] = false;
                                 }
+                                vis[s] = false;
                         }
-                        (ans *= fac[res]) %= mod;
+                        if (res >= (int)g[node].size() - 2) {
+                                (ans *= fac[res]) %= mod;
+                        } else {
+                                ans = 0;
+                        }
                         res = 0;
                 }
                 if (tag > 2) {
-                        (ans *= 4) %= mod;
-                } else {
                         (ans *= 2) %= mod;
                 }
 
                 return ans;
         };
 
-        ll ans = 1;
+        ll ans = m >= n ? 0 : 2;
         for (int i = 1; i <= n; ++i) {
                 if (vis[i]) {
                         (ans *= get_num(i)) %= mod;
---
+++ luogu/p12368.cc
@@ -0,0 +1,59 @@
+/*
+[[ ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷ ]],
+[[ ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇ ]],
+[[ ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽ ]],
+[[ ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕ ]],
+[[ ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕ ]],
+[[ ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕ ]],
+[[ ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄ ]],
+[[ ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕ ]],
+[[ ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿ ]],
+[[ ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ]],
+[[ ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟ ]],
+[[ ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠ ]],
+[[ ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙ ]],
+[[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
+*/
+/* #pragma GCC optimize(2) */
+#include <iostream>
+#include <deque>
+using namespace std;
+#define endl '\n'
+#define ll long long
+#define Debug(x) cout << #x << ':' << x << endl
+#define all(x) (x).begin(), (x).end()
+
+signed main() {
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+
+        string s;
+        cin >> s;
+        deque<char> st;
+        int res = 0;
+        int flag = 0;
+        for (char c : s) {
+                if (st.empty()) {
+                        st.push_back(c);
+                        flag = 1;
+                } else if (st.back() == c) {
+                        if (st.size() > 1 && flag == 1) {
+                                st.pop_back();
+                                st.pop_back();
+                        } else {
+                                flag = 2;
+                        }
+                        st.push_back(c);
+                } else {
+                        if (flag == 1) {
+                                st.pop_back();
+                                flag = 0;
+                        } else if (flag == 2) {
+                                st.pop_back();
+                                flag = 0;
+                        }
+                }
+        }
+
+        return 0;
+}
