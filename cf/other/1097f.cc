c14b1ae33592f7b465fdd766f48ea59be790a264
Kicamon <kicamonice@gmail.com>
2025-08-15 02:04:15 +0800

    update

--- cf/other/1097f.cc
+++ cf/other/1097f.cc
@@ -17,12 +17,14 @@
 /* #pragma GCC optimize(2) */
 #include <bitset>
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
 #define endl '\n'
 #define ll long long
 #define Debug(x) cout << #x << ':' << x << endl
 #define all(x) (x).begin(), (x).end()
+const int N = 7001;
 
 signed main() {
         ios::sync_with_stdio(false);
@@ -30,24 +32,40 @@ signed main() {
 
         int n, q;
         cin >> n >> q;
-        vector<bitset<7001> > a(n + 1);
+        vector<bitset<N> > a(n + 1), pr(N), pre(N);
+        bitset<N> mu;
+
+        mu.set();
+        for (int i = 2; i * i < N; ++i) {
+                for (int j = 1; i * i * j < N; ++j) {
+                        mu.reset(i * i * j);
+                }
+        }
+        for (int i = 1; i < N; ++i) {
+                for (int j = 1; i * j < N; ++j) {
+                        pr[i * j].set(i);
+                        pre[i][i * j] = mu[j];
+                }
+        }
+
         int op, x, y, v;
         while (q--) {
                 cin >> op;
                 if (op == 1) {
                         cin >> x >> v;
-                        a[x] = 0;
-                        a[x] |= 1 << v;
+                        a[x] = pr[v];
                 } else if (op == 2) {
                         cin >> x >> y >> v;
                         a[x] = a[y] ^ a[v];
-                } else if (op == 3) { // 如何计算gcd
+                } else if (op == 3) {
                         cin >> x >> y >> v;
+                        a[x] = a[y] & a[v];
                 } else {
                         cin >> x >> v;
-                        a[x].test(v + 1);
+                        cout << ((a[x] & pre[v]).count() & 1);
                 }
         }
+        cout << endl;
 
         return 0;
 }
