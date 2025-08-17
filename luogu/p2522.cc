a3f9a5011bc3859f427cd4b7da91b160a9e3add7
Kicamon <kicamonice@gmail.com>
2025-08-18 03:31:12 +0800

    update

---
+++ luogu/p2522.cc
@@ -0,0 +1,85 @@
+/*
+[[ ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷ ]],
+[[ ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇ ]],
+[[ ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽ ]],
+[[ ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕ ]],
+[[ ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕ ]],
+[[ ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕ ]],
+[[ ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄ ]],
+[[ ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕ ]],
+[[ ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿ ]],
+[[ ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ]],
+[[ ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟ ]],
+[[ ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠ ]],
+[[ ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙ ]],
+[[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
+*/
+/* #pragma GCC optimize(2) */
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define ll long long
+#define Debug(x) cout << #x << ':' << x << endl
+#define all(x) (x).begin(), (x).end()
+const int N = 5e4 + 10;
+vector<int> prime;
+array<int, N> mu;
+array<ll, N> sum;
+array<bool, N> st;
+
+void init() {
+        mu[1] = sum[1] = 1;
+        for (int i = 2; i < N; ++i) {
+                if (!st[i]) {
+                        prime.push_back(i);
+                        mu[i] = -1;
+                }
+                for (int x : prime) {
+                        if (x * i >= N) {
+                                break;
+                        }
+                        st[x * i] = true;
+                        if (i % x == 0) {
+                                break;
+                        }
+                        mu[x * i] = -mu[i];
+                }
+                sum[i] = sum[i - 1] + mu[i];
+        }
+}
+
+int g(int a, int b) {
+        return a / (a / b);
+}
+
+ll f(int x, int y, int k) {
+        x /= k, y /= k;
+        int n = min(x, y);
+        ll ans = 0;
+        for (int i = 1, t; i <= n; ++i) {
+                t = min(n, min(g(x, i), g(y, i)));
+                ans += (sum[t] - sum[i - 1]) * (x / i) * (y / i);
+                i = t;
+        }
+        return ans;
+}
+
+void solve() {
+        int a, b, c, d, k;
+        cin >> a >> b >> c >> d >> k;
+        cout << f(b, d, k) - f(a - 1, d, k) - f(b, c - 1, k) + f(a - 1, c - 1, k) << endl;
+}
+
+signed main() {
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+
+        init();
+        int t;
+        cin >> t;
+        while (t--) {
+                solve();
+        }
+
+        return 0;
+}
