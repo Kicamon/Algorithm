c864a23de6f501aab850c68c0001d7a404fd8e6d
Kicamon <kicamonice@gmail.com>
2025-08-18 23:24:42 +0800

    update

--- cf/other/1095/f.cc
+++ cf/other/1095/f.cc
@@ -15,12 +15,62 @@
 [[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
 */
 /* #pragma GCC optimize(2) */
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <vector>
 using namespace std;
 #define endl '\n'
 #define ll long long
 #define Debug(x) cout << #x << ':' << x << endl
 #define all(x) (x).begin(), (x).end()
+
+struct node {
+        ll val;
+        int x, y;
+        bool operator<(const node &t) {
+                return val < t.val;
+        }
+};
+
+struct DSU {
+        vector<int> f, siz;
+        vector<ll> val;
+
+        DSU(int n) {
+                f.resize(n);
+                iota(all(f), 0);
+                siz.assign(n, 1);
+                val.assign(n, 0);
+        }
+
+        int find(int x) {
+                while (x != f[x]) {
+                        x = f[x] = f[f[x]];
+                }
+                return x;
+        }
+
+        bool merge(int x, int y) {
+                x = find(x);
+                y = find(y);
+                if (x == y) {
+                        return false;
+                }
+                f[y] = x;
+                siz[x] += siz[y];
+                val[x] += val[y];
+                return true;
+        }
+
+        bool same(int x, int y) {
+                return find(x) == find(y);
+        }
+
+        bool size(int x) {
+                return siz[find(x)];
+        }
+};
 
 signed main() {
         ios::sync_with_stdio(false);
@@ -28,7 +78,8 @@ signed main() {
 
         int n, m;
         cin >> n >> m;
-        vector<ll> a(n), val(n);
+        DSU dsu(n);
+        vector<ll> a(n);
         int idx = 0;
         for (int i = 0; i < n; ++i) {
                 cin >> a[i];
@@ -36,31 +87,26 @@ signed main() {
                         idx = i;
                 }
         }
-        ll ans = 0;
+        vector<node> q;
         for (int i = 0; i < n; ++i) {
                 if (i == idx) {
                         continue;
                 }
-                val[i] = a[idx] + a[i];
-                ans += a[idx] + a[i];
+                q.push_back({ a[idx] + a[i], idx, i });
         }
-        for (ll i = 0, x, y, w; i < m; ++i) {
+        ll w;
+        for (int i = 0, x, y; i < m; ++i) {
                 cin >> x >> y >> w;
                 x--, y--;
-                if ((x == idx || y == idx) && w < a[x] + a[y]) {
-                        ans -= a[x] + a[y];
-                        ans += w;
-                } else if (w < max(val[x], val[y])) {
-                        ans -= max(val[x], val[y]);
-                        ans += w;
-                        if (val[x] > val[y]) {
-                                val[x] += val[y];
-                        } else {
-                                val[y] += val[x];
-                        }
+                q.push_back({ w, x, y });
+        }
+        sort(all(q));
+        for (node t : q) {
+                if (dsu.merge(t.x, t.y)) {
+                        dsu.val[dsu.find(t.x)] += t.val;
                 }
         }
-        cout << ans << endl;
+        cout << dsu.val[dsu.find(0)] << endl;
 
         return 0;
 }
