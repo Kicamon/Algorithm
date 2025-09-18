1d14d6b97f4fd0ef2f0785fc3487631c95c7b6dc
Kicamon <kicamonice@gmail.com>
2025-09-19 01:52:41 +0800

    update

--- xcpc/CCPC2025NanChang/i.cc
+++ xcpc/CCPC2025NanChang/i.cc
@@ -15,39 +15,40 @@
 [[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
 */
 /* #pragma GCC optimize(2) */
-#include <algorithm>
-#include <functional>
+#include <bitset>
 #include <iostream>
-#include <queue>
-#include <vector>
-#include <array>
 using namespace std;
 #define endl '\n'
 #define ll long long
 #define Debug(x) cout << #x << ':' << x << endl
 #define all(x) (x).begin(), (x).end()
-const int inf = 1e9;
+
+void solve() {
+        int n, k;
+        cin >> n >> k;
+        string s;
+        cin >> s;
+        int num = 0;
+        for (char c : s) {
+                if (c == '1') {
+                        num++;
+                }
+        }
+        if (num < k) {
+                cout << 0 << endl;
+                return;
+        }
+
+}
 
 signed main() {
         ios::sync_with_stdio(false);
         cin.tie(0);
 
-        int n, m, q;
-        cin >> n >> m >> q;
-        vector<vector<int> > a(n);
-        vector<ll> dis(n, inf);
-        vector<int> dep(n);
-        vector<vector<array<int, 2> > > g(n);
-        for (int i = 0, u, v, w; i < m; ++i) {
-                cin >> u >> v >> w;
-                u--, v--;
-                g[u].push_back({ v, w });
-        }
-
-        while (q--) {
-                int u, x;
-                cin >> u >> x;
-                u--;
+        int t;
+        cin >> t;
+        while (t--) {
+                solve();
         }
 
         return 0;
