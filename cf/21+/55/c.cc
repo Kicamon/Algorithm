c56d4afadea10a9813576e49e2303d914c2ae1c6
Kicamon <kicamonice@gmail.com>
2025-10-10 14:55:33 +0800

    2025-10-10 14:55:33

--- cf/21+/55/c.cc
+++ cf/21+/55/c.cc
@@ -15,6 +15,7 @@
 [[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
 */
 /* #pragma GCC optimize(2) */
+#include <functional>
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -22,7 +23,6 @@ using namespace std;
 #define ll long long
 #define Debug(x) cout << #x << ':' << x << endl
 #define all(x) (x).begin(), (x).end()
-const int mod = 676767677;
 
 void solve() {
         int n;
@@ -30,16 +30,57 @@ void solve() {
         vector<int> a(n);
         for (int &x : a) {
                 cin >> x;
-                x--;
         }
-        int l = 0, r = a.back();
-        ll ans = 0;
-        for (int i = 0; i < n; ++i) {
-                while (l + n - i - 1 < a[i]) {
-                        l++;
+        int ans = 2;
+        vector<bool> b(n);
+
+        function<bool()> check = [&]() {
+                vector<int> c(n, 1);
+                for (int i = 1, x = 0; i < n; ++i) {
+                        if (!b[i - 1]) {
+                                x++;
+                        }
+                        c[i] += x;
                 }
-                int L = min(l, i), R = min(r, n - i - 1);
+                for (int i = n - 2, x = 0; ~i; --i) {
+                        if (b[i + 1]) {
+                                x++;
+                        }
+                        c[i] += x;
+                }
+                return a == c;
+        };
+
+        for (int f = 0; f <= 1; ++f) {
+                b[0] = f;
+                bool flag = true;
+                for (int i = 1; i < n; ++i) {
+                        if (a[i - 1] == a[i]) {
+                                b[i] = !b[i - 1];
+                        } else if (a[i - 1] + 1 == a[i]) {
+                                if (b[i - 1]) {
+                                        ans--;
+                                        flag = false;
+                                        break;
+                                }
+                        } else if (a[i - 1] == a[i] + 1) {
+                                if (!b[i - 1]) {
+                                        ans--;
+                                        flag = false;
+                                        break;
+                                }
+                                b[i] = true;
+                        } else {
+                                ans--;
+                                flag = false;
+                                break;
+                        }
+                }
+                if (flag && !check()) {
+                        ans--;
+                }
         }
+        cout << ans << endl;
 }
 
 signed main() {
