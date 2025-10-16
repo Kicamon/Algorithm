5089d2e4a1d542a3b088748eaf8817435fc013b6
Kicamon <kicamonice@gmail.com>
2025-10-16 18:31:03 +0800

    2025.10.16 18:31:03

---
+++ yd/每日一题/1015.cc
@@ -0,0 +1,66 @@
+/*
+[[ ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷ ]],
+[[ ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇ ]],
+[[ ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽ ]],
+[[ ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕ ]],
+[[ ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕ ]],
+[[ ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕ ]],
+[[ ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄ ]],
+[[ ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕ ]],
+[[ ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿ ]],
+[[ ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ]],
+[[ ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟ ]],
+[[ ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠ ]],
+[[ ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙ ]],
+[[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
+*/
+/* #pragma GCC optimize(2) */
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <array>
+using namespace std;
+#define endl '\n'
+#define ll long long
+#define Debug(x) cout << #x << ':' << x << endl
+#define all(x) (x).begin(), (x).end()
+
+array<int, 2> mv[] = { { -2, -1 }, { -2, 1 }, { -1, 2 }, { 1, 2 },
+                       { 2, 1 },   { 2, -1 }, { 1, -2 }, { -1, -2 } };
+
+signed main() {
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+
+        int n, m, sx, sy;
+        cin >> n >> m >> sx >> sy;
+        sx--, sy--;
+        vector<vector<int>> g(n, vector<int>(m, -1));
+        g[sx][sy] = 0;
+
+        queue<array<int, 2>> q;
+        q.push({ sx, sy });
+        while (!q.empty()) {
+                auto [x, y] = q.front();
+                q.pop();
+                for (int i = 0; i < 8; ++i) {
+                        int X = x + mv[i][0], Y = y + mv[i][1];
+                        if (X < 0 || X >= n || Y < 0 || Y >= m) {
+                                continue;
+                        }
+                        if (g[X][Y] == -1 || g[x][y] + 1 < g[X][Y]) {
+                                g[X][Y] = g[x][y] + 1;
+                                q.push({ X, Y });
+                        }
+                }
+        }
+
+        for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < m; ++j) {
+                        cout << g[i][j] << " \n"[j == m - 1];
+                }
+        }
+
+        return 0;
+}
