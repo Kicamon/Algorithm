6b915c4dba2bada3b8add8644716362f9378f1eb
Kicamon <kicamonice@gmail.com>
2025-10-21 18:47:26 +0800

    2025.10.21 18:47:26

---
+++ luogu/drill/8.图论/p3355.cc
@@ -0,0 +1,92 @@
+/*
+[[ ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷ ]],
+[[ ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇ ]],
+[[ ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽ ]],
+[[ ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕ ]],
+[[ ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕ ]],
+[[ ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕ ]],
+[[ ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄ ]],
+[[ ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕ ]],
+[[ ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿ ]],
+[[ ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ]],
+[[ ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟ ]],
+[[ ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠ ]],
+[[ ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙ ]],
+[[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
+*/
+/* #pragma GCC optimize(2) */
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <numeric>
+#include <vector>
+using namespace std;
+#define endl '\n'
+#define ll long long
+#define Debug(x) cout << #x << ':' << x << endl
+#define all(x) (x).begin(), (x).end()
+
+struct DSU {
+        vector<int> f, siz;
+
+        DSU(int n) {
+                f.resize(n);
+                iota(all(f), 0);
+                siz.assign(n, 1);
+        }
+
+        int find(int x) {
+                while (x != f[x]) {
+                        x = f[x] = f[f[x]];
+                }
+                return x;
+        }
+
+        bool merge(int x, int y) {
+                x = find(x);
+                y = find(y);
+                if (x == y) {
+                        return false;
+                }
+                f[y] = x;
+                siz[x] += siz[y];
+                return true;
+        }
+
+        bool same(int x, int y) {
+                return find(x) == find(y);
+        }
+
+        int size(int x) {
+                return siz[find(x)];
+        }
+};
+
+signed main() {
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+
+        int n, m;
+        cin >> n >> m;
+        DSU dsu(n);
+        vector<array<int, 3>> a(m);
+        for (int i = 0, u, v, w; i < m; ++i) {
+                cin >> u >> v >> w;
+                u--, v--;
+                a[i] = { u, v, w };
+        }
+        sort(all(a), [](auto x, auto y) { return x[2] < y[2]; });
+        int ans = 0;
+        for (auto [u, v, w] : a) {
+                if (dsu.merge(u, v)) {
+                        ans += w;
+                }
+        }
+        if (dsu.size(0) < n) {
+                cout << "orz" << endl;
+        } else {
+                cout << ans << endl;
+        }
+
+        return 0;
+}
---
+++ luogu/drill/8.图论/p4180.cc
@@ -0,0 +1,90 @@
+/*
+[[ ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷ ]],
+[[ ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇ ]],
+[[ ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽ ]],
+[[ ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕ ]],
+[[ ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕ ]],
+[[ ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕ ]],
+[[ ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄ ]],
+[[ ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕ ]],
+[[ ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿ ]],
+[[ ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ]],
+[[ ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟ ]],
+[[ ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠ ]],
+[[ ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙ ]],
+[[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
+*/
+/* #pragma GCC optimize(2) */
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <vector>
+#include <array>
+using namespace std;
+#define endl '\n'
+#define ll long long
+#define Debug(x) cout << #x << ':' << x << endl
+#define all(x) (x).begin(), (x).end()
+
+struct DSU {
+        vector<int> f, siz;
+
+        DSU(int n) {
+                f.resize(n);
+                iota(all(f), 0);
+                siz.assign(n, 1);
+        }
+
+        int find(int x) {
+                while (x != f[x]) {
+                        x = f[x] = f[f[x]];
+                }
+                return x;
+        }
+
+        bool merge(int x, int y) {
+                x = find(x);
+                y = find(y);
+                if (x == y) {
+                        return false;
+                }
+                f[y] = x;
+                siz[x] += siz[y];
+                return true;
+        }
+
+        bool same(int x, int y) {
+                return find(x) == find(y);
+        }
+
+        int size(int x) {
+                return siz[find(x)];
+        }
+};
+
+signed main() {
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+
+        int n, m;
+        cin >> n >> m;
+        DSU dsu(n);
+        vector<array<int, 3>> a(m);
+        for (int i = 0, u, v, w; i < m; ++i) {
+                cin >> u >> v >> w;
+                u--, v--;
+                a[i] = { u, v, w };
+        }
+        sort(all(a), [](const auto &x, const auto &y) { return x[2] < y[2]; });
+        int inf = 1e9;
+        ll ans = 0;
+        for (const auto &[u, v, w] : a) {
+                if (dsu.merge(u, v)) {
+                        ans += w;
+                } else {
+                        inf = min(inf, w);
+                }
+        }
+
+        return 0;
+}
