01593dc438ad2a4c339a61875efca7061f0e48cc
Kicamon <kicamonice@gmail.com>
2025-10-24 19:03:12 +0800

    2025.10.24 19:03:12

--- luogu/drill/8.图论/p4180.cc
+++ luogu/drill/8.图论/p4180.cc
@@ -16,6 +16,7 @@
 */
 /* #pragma GCC optimize(2) */
 #include <algorithm>
+#include <climits>
 #include <iostream>
 #include <numeric>
 #include <vector>
@@ -26,13 +27,28 @@ using namespace std;
 #define Debug(x) cout << #x << ':' << x << endl
 #define all(x) (x).begin(), (x).end()
 
-struct DSU {
-        vector<int> f, siz;
+class TR {
+    private:
+        const int inf = INT_MAX;
+        const ll INF = LLONG_MAX;
+        vector<vector<array<int, 2>>> g;
+        vector<vector<int>> fa, mmax, smax;
+        vector<int> dep, f;
+        vector<bool> vis;
+        int max_dep;
+        vector<array<int, 3>> edge;
+        int n, m;
+        ll sum;
 
-        DSU(int n) {
-                f.resize(n);
+    public:
+        TR(int _n, int _m) {
+                n = _n, m = _m;
+                max_dep = __lg(n) + 1;
+                g.resize(n + 1), dep.resize(n + 1), vis.resize(n + 1), f.resize(n + 1);
+                fa.assign(n + 1, vector<int>(max_dep, 0));
+                mmax.assign(n + 1, vector<int>(max_dep, -inf));
+                smax.assign(n + 1, vector<int>(max_dep, -inf));
                 iota(all(f), 0);
-                siz.assign(n, 1);
         }
 
         int find(int x) {
@@ -49,16 +65,105 @@ struct DSU {
                         return false;
                 }
                 f[y] = x;
-                siz[x] += siz[y];
                 return true;
         }
 
-        bool same(int x, int y) {
-                return find(x) == find(y);
+        void kruskal() {
+                sort(all(edge));
+                for (int i = 0; i < m; ++i) {
+                        const auto &[u, v, w] = edge[i];
+                        if (merge(u, v)) {
+                                sum += w;
+                                g[u].push_back({ v, w });
+                                g[v].push_back({ u, w });
+                                vis[i] = true;
+                        }
+                }
         }
 
-        int size(int x) {
-                return siz[find(x)];
+        void add_edge(int u, int v, int w) {
+                edge.push_back({ u, v, w });
+        }
+
+        void dfs(int u, int p) {
+                fa[u][0] = p;
+                dep[u] = dep[p] + 1;
+                for (int i = 1; (1 << i) <= dep[u]; ++i) {
+                        fa[u][i] = fa[fa[u][i - 1]][i - 1];
+                        int v = fa[u][i - 1];
+                        int tmp[4]
+                            = { mmax[u][i - 1], smax[u][i - 1], mmax[v][i - 1], smax[v][i - 1] };
+                        sort(tmp, tmp + 4);
+                        mmax[u][i] = tmp[3];
+                        int idx = 2;
+                        while (~idx && tmp[idx] == tmp[3]) {
+                                --idx;
+                        }
+                        if (idx != -1) {
+                                smax[u][i] = tmp[idx];
+                        }
+                }
+
+                for (const auto &[v, w] : g[u]) {
+                        if (v == p) {
+                                continue;
+                        }
+                        mmax[v][0] = w;
+                        dfs(v, u);
+                }
+        }
+
+        int lca(int a, int b) {
+                if (dep[a] < dep[b]) {
+                        swap(a, b);
+                }
+                for (int i = max_dep; ~i; --i) {
+                        if (dep[fa[a][i]] >= dep[b]) {
+                                a = fa[a][i];
+                        }
+                }
+                if (a == b) {
+                        return a;
+                }
+                for (int i = max_dep; ~i; --i) {
+                        if (fa[a][i] != fa[b][i]) {
+                                a = fa[a][i];
+                                b = fa[b][i];
+                        }
+                }
+                return fa[a][0];
+        }
+
+        int query(int x, int y, int w) {
+                int res = -inf;
+                for (int i = max_dep; ~i; --i) {
+                        if (w != mmax[x][i]) {
+                                res = max(res, mmax[x][i]);
+                        } else {
+                                res = max(res, smax[x][i]);
+                        }
+                }
+                return res;
+        }
+
+        ll tr() {
+                kruskal();
+                ll ans = INF;
+                dfs(1, 0);
+
+                for (int i = 0; i < m; ++i) {
+                        if (vis[i]) {
+                                continue;
+                        }
+                        const auto &[u, v, w] = edge[i];
+                        int _lca = lca(u, v);
+                        ll tmpa = query(u, _lca, w);
+                        ll tmpb = query(v, _lca, w);
+                        if (max(tmpa, tmpb) > -inf) {
+                                ans = min(ans, sum - max(tmpa, tmpb) + w);
+                        }
+                }
+                return ans == INF ? -1 : ans;
         }
 };
 
@@ -68,23 +173,12 @@ signed main() {
 
         int n, m;
         cin >> n >> m;
-        DSU dsu(n);
-        vector<array<int, 3>> a(m);
+        TR tr(n, m);
         for (int i = 0, u, v, w; i < m; ++i) {
                 cin >> u >> v >> w;
-                u--, v--;
-                a[i] = { u, v, w };
+                tr.add_edge(u, v, w);
         }
-        sort(all(a), [](const auto &x, const auto &y) { return x[2] < y[2]; });
-        int inf = 1e9;
-        ll ans = 0;
-        for (const auto &[u, v, w] : a) {
-                if (dsu.merge(u, v)) {
-                        ans += w;
-                } else {
-                        inf = min(inf, w);
-                }
-        }
+        cout << tr.tr() << endl;
 
         return 0;
 }
