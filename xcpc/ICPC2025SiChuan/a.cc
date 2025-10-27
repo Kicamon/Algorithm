64090e386029c304439326984627d56dcd6ff88d
Kicamon <kicamonice@gmail.com>
2025-10-27 18:28:22 +0800

    2025.10.27 18:28:21

---
+++ xcpc/ICPC2025SiChuan/a.cc
@@ -0,0 +1,108 @@
+/*
+[[ ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷ ]],
+[[ ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇ ]],
+[[ ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽ ]],
+[[ ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕ ]],
+[[ ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕ ]],
+[[ ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕ ]],
+[[ ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄ ]],
+[[ ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕ ]],
+[[ ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿ ]],
+[[ ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ]],
+[[ ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟ ]],
+[[ ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠ ]],
+[[ ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙ ]],
+[[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
+*/
+/* #pragma GCC optimize(2) */
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <array>
+using namespace std;
+#define endl '\n'
+#define ll long long
+#define Debug(x) cout << #x << ':' << x << endl
+#define all(x) (x).begin(), (x).end()
+const int inf = 0x3f3f3f3f;
+
+class node {
+    public:
+        int a, b;
+
+        bool operator<(const node &t) {
+                if (a * b != t.a * t.b) {
+                        return a * b < t.a * t.b;
+                }
+                if (a != t.a) {
+                        return a < t.a;
+                }
+                return b < t.b;
+        }
+
+        bool operator>(const node &t) const {
+                if (a * b != t.a * t.b) {
+                        return a * b > t.a * t.b;
+                }
+                if (a != t.a) {
+                        return a > t.a;
+                }
+                return b > t.b;
+        }
+};
+
+class tp : public node {
+    public:
+        int p;
+};
+
+void solve() {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<array<int, 3>>> g(n + 1);
+        vector<node> dis(n + 1, { inf, inf });
+        vector<bool> vis(n + 1);
+        for (int i = 0, u, v, a, b; i < m; ++i) {
+                cin >> u >> v >> a >> b;
+                g[u].push_back({ v, a, b });
+        }
+
+        function<void()> dijskstra = [&]() {
+                priority_queue<tp, vector<tp>, greater<tp>> q;
+                q.push(tp{ 0, 0, 1 });
+                dis[1] = { 0, 0 };
+                while (!q.empty()) {
+                        int u = q.top().p;
+                        q.pop();
+                        // if (vis[u]) {
+                        //         continue;
+                        // }
+                        // vis[u] = true;
+                        for (const auto &[v, a, b] : g[u]) {
+                                node t = { dis[u].a + a, dis[u].b + b };
+                                if (t < dis[v]) {
+                                        dis[v] = t;
+                                        q.push({ dis[v].a, dis[v].b, v });
+                                }
+                        }
+                }
+        };
+
+        dijskstra();
+
+        cout << dis[n].a << ' ' << dis[n].b << endl;
+}
+
+signed main() {
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+
+        int t;
+        cin >> t;
+        while (t--) {
+                solve();
+        }
+
+        return 0;
+}
---
+++ xcpc/ICPC2025SiChuan/f.cc
@@ -0,0 +1,84 @@
+/*
+[[ ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷ ]],
+[[ ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇ ]],
+[[ ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽ ]],
+[[ ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕ ]],
+[[ ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕ ]],
+[[ ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕ ]],
+[[ ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄ ]],
+[[ ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕ ]],
+[[ ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿ ]],
+[[ ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ]],
+[[ ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟ ]],
+[[ ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠ ]],
+[[ ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙ ]],
+[[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
+*/
+/* #pragma GCC optimize(2) */
+#include <iostream>
+#include <vector>
+using namespace std;
+#define endl '\n'
+#define ll long long
+#define Debug(x) cout << #x << ':' << x << endl
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        s = ' ' + s;
+        vector<int> a(n + 2), pre(n + 1), suf(n + 2);
+        for (int i = 1; i <= n; ++i) {
+                if (s[i] == '1') {
+                        a[i] = 1;
+                } else {
+                        a[i] = 0;
+                }
+        }
+        a[n + 1] = -1;
+        for (int i = n; i; --i) {
+                suf[i] = suf[i + 1];
+                if (!a[i]) {
+                        suf[i]++;
+                }
+        }
+        for (int i = 1; i <= n; ++i) {
+                pre[i] = pre[i - 1];
+                if (s[i] == '?') {
+                        if (pre[i] < suf[i + 1]) {
+                                pre[i]++;
+                                a[i] = 1;
+                        }
+                } else if (s[i] == '1') {
+                        pre[i]++;
+                }
+        }
+        for (int i = n; i; --i) {
+                suf[i] = suf[i + 1];
+                if (!a[i + 1]) {
+                        suf[i]++;
+                }
+        }
+        ll ans = 0;
+        for (int i = 1; i <= n; ++i) {
+                if (a[i]) {
+                        ans += suf[i];
+                }
+        }
+        cout << ans << endl;
+}
+
+signed main() {
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+
+        int t;
+        cin >> t;
+        while (t--) {
+                solve();
+        }
+
+        return 0;
+}
