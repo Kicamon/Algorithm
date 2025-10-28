6216759dbcd07009c96b891e5d34c260b57c4c1f
Kicamon <kicamonice@gmail.com>
2025-10-28 18:48:05 +0800

    2025.10.28 18:48:05

---
+++ xcpc/ICPC2025SiChuan/j.cc
@@ -0,0 +1,90 @@
+/*
+[[ ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷ ]],
+[[ ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇ ]],
+[[ ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽ ]],
+[[ ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕ ]],
+[[ ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕ ]],
+[[ ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕ ]],
+[[ ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄ ]],
+[[ ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕ ]],
+[[ ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿ ]],
+[[ ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ]],
+[[ ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟ ]],
+[[ ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠ ]],
+[[ ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙ ]],
+[[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
+*/
+/* #pragma GCC optimize(2) */
+#include <iostream>
+#include <vector>
+using namespace std;
+#define endl '\n'
+#define ll long long
+#define Debug(x) cout << #x << ':' << x << endl
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+        int n;
+        string s;
+        cin >> n >> s;
+        s = ' ' + s;
+        vector<vector<int>> g(n + 1);
+        vector<int> num_s(n + 1), num_p(n + 1), num_c(n + 1);
+        vector<bool> vis(n + 1);
+        for (int i = 1, u, v; i < n; ++i) {
+                cin >> u >> v;
+                g[u].push_back(v);
+                g[v].push_back(u);
+        }
+        for (int u = 1; u <= n; ++u) {
+                if (s[u] == 'C') {
+                        for (int v : g[u]) {
+                                num_s[u] += s[v] == 'S';
+                        }
+                }
+                if (s[u] == 'P') {
+                        for (int v : g[u]) {
+                                num_c[u] += s[v] == 'C';
+                        }
+                        num_c[u] -= (num_c[u] > 0);
+                }
+        }
+        for (int u = 1; u <= n; ++u) {
+                if (s[u] != 'C') {
+                        continue;
+                }
+                for (int v : g[u]) {
+                        if (s[v] == 'P') {
+                                num_p[u] += num_c[v];
+                        }
+                }
+        }
+        ll ans = 0;
+        for (int u = 1; u <= n; ++u) {
+                if (s[u] != 'C') {
+                        continue;
+                }
+                for (int v : g[u]) {
+                        if (vis[v] || s[v] != 'C') {
+                                continue;
+                        }
+                        ans += 1ll * num_s[u] * num_p[v];
+                        ans += 1ll * num_p[u] * num_s[v];
+                        vis[u] = true;
+                }
+        }
+        cout << ans << endl;
+}
+
+signed main() {
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+
+        int t;
+        cin >> t;
+        while (t--) {
+                solve();
+        }
+
+        return 0;
+}
