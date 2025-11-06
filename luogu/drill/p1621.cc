ecf2a8679ecb76e5ee83f17c2c7a56fd9fab7ea5
Kicamon <kicamonice@gmail.com>
2025-11-06 20:25:46 +0800

    2025.11.06 20:25:46

---
+++ luogu/drill/p1621.cc
@@ -0,0 +1,114 @@
+/*
+[[ ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷ ]],
+[[ ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇ ]],
+[[ ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽ ]],
+[[ ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕ ]],
+[[ ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕ ]],
+[[ ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕ ]],
+[[ ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄ ]],
+[[ ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕ ]],
+[[ ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿ ]],
+[[ ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ]],
+[[ ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟ ]],
+[[ ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠ ]],
+[[ ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙ ]],
+[[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
+*/
+/* #pragma GCC optimize(2) */
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <vector>
+using namespace std;
+#define endl '\n'
+#define ll long long
+#define Debug(x) cout << #x << ':' << x << endl
+#define all(x) (x).begin(), (x).end()
+
+class DSU {
+    private:
+        vector<int> f;
+
+    public:
+        DSU(int n) {
+                f.resize(n);
+                iota(all(f), 0);
+        }
+
+        int find(int x) {
+                while (x != f[x]) {
+                        x = f[x] = f[f[x]];
+                }
+                return x;
+        }
+
+        bool merge(int x, int y) {
+                x = find(x);
+                y = find(y);
+                if (x == y) {
+                        return false;
+                }
+                f[y] = x;
+                return true;
+        }
+
+        bool same(int x, int y) {
+                return find(x) == find(y);
+        }
+};
+
+vector<set<int>> pt;
+
+void prime(int n, int p) {
+        pt.resize(n + 1);
+        vector<int> primes;
+        vector<bool> vis(n + 1);
+        for (int i = 2; i <= n; ++i) {
+                if (!vis[i]) {
+                        primes.push_back(i);
+                        if (i >= p) {
+                                pt[i].insert(i);
+                        }
+                }
+                for (int j : primes) {
+                        if (i * j > n) {
+                                break;
+                        }
+                        vis[i * j] = true;
+                        pt[i * j] = pt[i];
+                        if (j >= p) {
+                                pt[i * j].insert(j);
+                        }
+                        if (i % j == 0) {
+                                break;
+                        }
+                }
+        }
+}
+
+signed main() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        int l, r, p;
+        cin >> l >> r >> p;
+        DSU dsu(r + 1);
+        prime(r, p);
+        for (int i = l; i <= r; ++i) {
+                for (int j : pt[i]) {
+                        dsu.merge(i, j);
+                }
+        }
+        int ans = 0;
+        vector<bool> vis(r + 1);
+        for (int i = l; i <= r; ++i) {
+                int t = dsu.find(i);
+                if (!vis[t]) {
+                        ans++;
+                }
+                vis[t] = true;
+        }
+        cout << ans << endl;
+
+        return 0;
+}
