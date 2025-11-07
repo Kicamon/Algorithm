2c988b28624f53c312d68300b8cb66b0ab333355
Kicamon <kicamonice@gmail.com>
2025-11-07 18:33:31 +0800

    2025.11.07 18:33:31

---
+++ luogu/drill/p10930.cc
@@ -0,0 +1,84 @@
+/*
+[[ ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷ ]],
+[[ ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇ ]],
+[[ ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽ ]],
+[[ ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕ ]],
+[[ ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕ ]],
+[[ ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕ ]],
+[[ ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄ ]],
+[[ ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕ ]],
+[[ ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿ ]],
+[[ ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ]],
+[[ ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟ ]],
+[[ ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠ ]],
+[[ ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙ ]],
+[[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
+*/
+/* #pragma GCC optimize(2) */
+#include <functional>
+#include <iostream>
+#include <vector>
+#include <array>
+using namespace std;
+#define endl '\n'
+#define ll long long
+#define Debug(x) cout << #x << ':' << x << endl
+#define all(x) (x).begin(), (x).end()
+
+signed main() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        int n;
+        cin >> n;
+        vector<vector<array<int, 2>>> g(n + 1);
+        vector<int> fa(n + 1), ud(n + 1);
+        vector<ll> dis(n + 1);
+        for (int i = 1, u, v, w; i < n; ++i) {
+                cin >> u >> v >> w;
+                g[u].push_back({ v, w });
+                g[v].push_back({ u, w });
+        }
+
+        function<void(int, int)> dfs = [&](int u, int p) {
+                fa[u] = p;
+                for (auto [v, w] : g[u]) {
+                        if (v == p) {
+                                continue;
+                        }
+                        dis[v] = dis[u] + w;
+                        dfs(v, u);
+                }
+        };
+
+        ud[1] = 1;
+        dfs(1, 0);
+
+        int m;
+        cin >> m;
+        ll ans = 0;
+        while (m--) {
+                char opt;
+                int x;
+                cin >> opt;
+                if (opt == '+') {
+                        cin >> x;
+                        opt = 1;
+                } else if (opt == '-') {
+                        cin >> x;
+                        opt = -1;
+                } else {
+                        cout << ans << endl;
+                        continue;
+                }
+                ans += dis[x] * opt;
+                while (ud[x] == ud[fa[x]]) {
+                        x = fa[x];
+                        ud[x] += opt;
+                }
+                x = fa[x];
+                ans -= dis[x] * opt;
+        }
+
+        return 0;
+}
