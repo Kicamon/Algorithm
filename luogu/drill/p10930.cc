7aa39bc44c243ccec8ea30de6d3fe8e2beb6341e
Kicamon <kicamonice@gmail.com>
2025-11-10 18:37:58 +0800

    2025.11.10 18:37:58

--- luogu/drill/p10930.cc
+++ luogu/drill/p10930.cc
@@ -17,13 +17,74 @@
 /* #pragma GCC optimize(2) */
 #include <functional>
 #include <iostream>
+#include <set>
+#include <tuple>
 #include <vector>
-#include <array>
 using namespace std;
 #define endl '\n'
 #define ll long long
 #define Debug(x) cout << #x << ':' << x << endl
 #define all(x) (x).begin(), (x).end()
+
+vector<vector<tuple<int, int>>> g;
+vector<vector<int>> fa;
+vector<int> dep, in;
+vector<ll> dis;
+int max_dep;
+int time_ptr;
+
+void init(int n) {
+        max_dep = __lg(n) + 1;
+        dep.resize(n + 1), in.resize(n + 1), g.resize(n + 1), dis.resize(n + 1);
+        fa.assign(n + 1, vector<int>(max_dep + 1));
+}
+
+void add_edge(int u, int v, int w) {
+        g[u].push_back({ v, w });
+        g[v].push_back({ u, w });
+}
+
+void dfs(int u, int p) {
+        in[u] = ++time_ptr;
+        dep[u] = dep[p] + 1;
+        fa[u][0] = p;
+        for (int i = 1; i <= max_dep; ++i) {
+                fa[u][i] = fa[fa[u][i - 1]][i - 1];
+        }
+        for (auto [v, w] : g[u]) {
+                if (v == p) {
+                        continue;
+                }
+                dis[v] = dis[u] + w;
+                dfs(v, u);
+        }
+}
+
+int lca(int a, int b) {
+        if (dep[a] < dep[b]) {
+                swap(a, b);
+        }
+        for (int i = max_dep; ~i; --i) {
+                if (dep[fa[a][i]] >= dep[b]) {
+                        a = fa[a][i];
+                }
+        }
+        if (a == b) {
+                return a;
+        }
+        for (int i = max_dep; ~i; --i) {
+                if (fa[a][i] != fa[b][i]) {
+                        a = fa[a][i];
+                        b = fa[b][i];
+                }
+        }
+        return fa[a][0];
+}
+
+ll get_dis(int u, int v) {
+        int p = lca(u, v);
+        return dis[u] + dis[v] - dis[p] * 2;
+}
 
 signed main() {
         ios::sync_with_stdio(false);
@@ -31,53 +92,58 @@ signed main() {
 
         int n;
         cin >> n;
-        vector<vector<array<int, 2>>> g(n + 1);
-        vector<int> fa(n + 1), ud(n + 1);
-        vector<ll> dis(n + 1);
+        init(n);
         for (int i = 1, u, v, w; i < n; ++i) {
                 cin >> u >> v >> w;
-                g[u].push_back({ v, w });
-                g[v].push_back({ u, w });
+                add_edge(u, v, w);
         }
-
-        function<void(int, int)> dfs = [&](int u, int p) {
-                fa[u] = p;
-                for (auto [v, w] : g[u]) {
-                        if (v == p) {
-                                continue;
-                        }
-                        dis[v] = dis[u] + w;
-                        dfs(v, u);
-                }
-        };
-
-        ud[1] = 1;
         dfs(1, 0);
-
         int m;
         cin >> m;
+
+        auto comp = [&](int a, int b) { return in[a] < in[b]; };
+        set<int, decltype(comp)> st(comp);
+        using sii = set<int>::iterator;
+        function<array<sii, 2>(sii)> itop = [&](sii it) {
+                auto left = it, right = it;
+                if (left == st.begin()) {
+                        left = st.end();
+                }
+                left--;
+                right++;
+                if (right == st.end()) {
+                        right = st.begin();
+                }
+
+                return array<sii, 2>{ left, right };
+        };
+
         ll ans = 0;
         while (m--) {
                 char opt;
-                int x;
                 cin >> opt;
                 if (opt == '+') {
+                        int x;
                         cin >> x;
-                        opt = 1;
+                        if (st.empty()) {
+                                st.insert(x);
+                                continue;
+                        }
+                        auto [left, right] = itop(st.insert(x).first);
+                        ans -= get_dis(*left, *right);
+                        ans += get_dis(*left, x) + get_dis(x, *right);
                 } else if (opt == '-') {
+                        int x;
                         cin >> x;
-                        opt = -1;
+                        auto it = st.find(x);
+                        auto [left, right] = itop(it);
+                        ans += get_dis(*left, *right);
+                        ans -= get_dis(*left, x) + get_dis(x, *right);
+
+                        st.erase(it);
                 } else {
-                        cout << ans << endl;
-                        continue;
+                        cout << (ans >> 1) << endl;
                 }
-                ans += dis[x] * opt;
-                while (ud[x] == ud[fa[x]]) {
-                        x = fa[x];
-                        ud[x] += opt;
-                }
-                x = fa[x];
-                ans -= dis[x] * opt;
         }
 
         return 0;
