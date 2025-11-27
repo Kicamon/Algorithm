93ff3c983b7d6a180de53274fc874d1680408040
Kicamon <kicamonice@gmail.com>
2025-11-27 18:30:34 +0800

    2025.11.27 18:30:34

--- luogu/drill/p11663.cc
+++ luogu/drill/p11663.cc
@@ -15,6 +15,7 @@
 [[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
 */
 /* #pragma GCC optimize(2) */
+#include <algorithm>
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -23,18 +24,64 @@ using namespace std;
 #define Debug(x) cout << #x << ':' << x << endl
 #define all(x) (x).begin(), (x).end()
 
+int n, m;
+struct Node {
+        int idx;
+        ll sum;
+};
+
+vector<Node> node;
+vector<int> a, b;
+
+bool check(int x, int y) {
+        return a[x] - b[x] > a[y] - b[y];
+}
+
+void push_up(int u) {
+        Node &l = node[u << 1], &r = node[u << 1 | 1];
+        node[u].idx = check(l.idx, r.idx) ? l.idx : r.idx;
+        node[u].sum = l.sum + r.sum;
+}
+
+void build(int u, int l, int r) {
+        if (l == r) {
+                node[u] = { l, b[l] };
+                return;
+        }
+        int mid = (l + r) >> 1;
+        build(u << 1, l, mid), build(u << 1 | 1, mid + 1, r);
+        push_up(u);
+}
+
+Node search(int u, int l, int r, int L, int R) {
+        if (L <= l && r <= R) {
+                return node[u];
+        }
+        int mid = (l + r) >> 1;
+        auto t = search(u << 1, l, mid, L, R);
+        auto p = search(u << 1 | 1, mid + 1, r, L, R);
+        return check(t.idx, p.idx) ? t : p;
+}
+
 signed main() {
         ios::sync_with_stdio(false);
         cin.tie(nullptr);
 
-        int n;
         cin >> n;
-        vector<int> a(n), b(n);
+        m = n << 1;
+        node.resize(m << 2), a.resize(m), b.resize(m);
+        int ans = 0;
         for (int i = 0; i < n; ++i) {
                 cin >> a[i];
+                a[i + n] = a[i];
+                ans = max(ans, a[i]);
         }
         for (int i = 0; i < n; ++i) {
                 cin >> b[i];
+                b[i + n] = b[i];
+        }
+        build(1, 1, m);
+        for (int i = 0; i < n; ++i) {
         }
 
         return 0;
