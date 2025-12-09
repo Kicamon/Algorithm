61f3878310dcb0d6260f57adf3d801e0143ef852
Kicamon <kicamonice@gmail.com>
2025-12-09 18:41:57 +0800

    2025.12.09 18:41:56

---
+++ luogu/drill/p1835.cc
@@ -0,0 +1,72 @@
+/*
+[[ ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷ ]],
+[[ ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇ ]],
+[[ ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽ ]],
+[[ ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕ ]],
+[[ ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕ ]],
+[[ ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕ ]],
+[[ ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄ ]],
+[[ ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕ ]],
+[[ ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿ ]],
+[[ ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ]],
+[[ ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟ ]],
+[[ ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠ ]],
+[[ ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙ ]],
+[[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
+*/
+/* #pragma GCC optimize(2) */
+#include <cmath>
+#include <iostream>
+#include <vector>
+using namespace std;
+#define endl '\n'
+#define ll long long
+#define Debug(x) cout << #x << ':' << x << endl
+#define all(x) (x).begin(), (x).end()
+
+vector<int> primes;
+void prime(int n) {
+        vector<bool> vis(n + 1);
+        for (int i = 2; i <= n; ++i) {
+                if (!vis[i]) {
+                        primes.push_back(i);
+                }
+                for (int j : primes) {
+                        if (i * j > n) {
+                                break;
+                        }
+                        vis[i * j] = true;
+                        if (i % j == 0) {
+                                break;
+                        }
+                }
+        }
+}
+
+signed main() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        prime(1e6);
+        int l, r;
+        cin >> l >> r;
+        int ans = r - l + (l != 1);
+        vector<bool> vis(r - l + 1);
+
+        for (int i : primes) {
+                if (i > l) {
+                        break;
+                }
+                ll t = ceil((double)l / i);
+                while (t * i <= r) {
+                        if (!vis[t * i - l]) {
+                                ans--;
+                                vis[t * i - l] = true;
+                        }
+                        t++;
+                }
+        }
+        cout << ans << '\n';
+
+        return 0;
+}
