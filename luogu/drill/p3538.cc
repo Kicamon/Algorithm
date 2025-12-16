93946f8e5296b68cc621ea665b2edfccd9e834aa
Kicamon <kicamonice@gmail.com>
2025-12-16 18:59:19 +0800

    2025.12.16 18:59:19

---
+++ luogu/drill/p3538.cc
@@ -0,0 +1,84 @@
+/*
+[[ ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷ ]],
+[[ ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇ ]],
+[[ ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽ ]],
+[[ ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕ ]],
+[[ ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕ ]],
+[[ ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕ ]],
+[[ ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄ ]],
+[[ ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕ ]],
+[[ ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿ ]],
+[[ ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ]],
+[[ ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟ ]],
+[[ ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠ ]],
+[[ ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙ ]],
+[[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
+*/
+/* #pragma GCC optimize(2) */
+#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+#define endl '\n'
+#define Debug(x) cout << #x << ':' << x << endl
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using ull = unsigned long long;
+const ull p = 18446744073709551533ull;
+
+int n;
+string s;
+vector<ull> a, mod;
+vector<int> primes;
+vector<set<int>> primes_s;
+
+void prime(int n) {
+        vector<bool> vis(n + 1);
+        for (int i = 2; i <= n; ++i) {
+                if (!vis[i]) {
+                        primes.push_back(i);
+                        primes_s[i].insert(i);
+                }
+                for (int j : primes) {
+                        if (i * j > n) {
+                                break;
+                        }
+                        vis[i * j] = true;
+                        primes_s[i * j] = primes_s[i];
+                        primes_s[j].insert(j);
+                        if (i % j == 0) {
+                                break;
+                        }
+                }
+        }
+}
+
+void solve() {
+        int l, r;
+        cin >> l >> r;
+        ull s = (a[r] - a[l - 1]) / mod[l - 1];
+}
+
+signed main() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        cin >> n >> s;
+
+        prime(n);
+
+        a.resize(n + 1), mod.resize(n + 1);
+        mod[0] = 1;
+        for (int i = 1; i <= n; ++i) {
+                mod[i] = mod[i - 1] * p;
+                a[i] = (s[i - 1] - 'a') * mod[i] + a[i - 1];
+        }
+
+        int q;
+        cin >> q;
+        while (q--) {
+                solve();
+        }
+
+        return 0;
+}
