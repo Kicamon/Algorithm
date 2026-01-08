176ce5f3a0ee16fc3dd48b26fb4da36afe1a8fdf
Kicamon <kicamonice@gmail.com>
2026-01-08 18:45:37 +0800

    2026.01.08 18:45:37

---
+++ nc/周赛/121/f.cc
@@ -0,0 +1,90 @@
+/*
+[[ ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷ ]],
+[[ ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇ ]],
+[[ ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽ ]],
+[[ ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕ ]],
+[[ ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕ ]],
+[[ ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕ ]],
+[[ ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄ ]],
+[[ ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕ ]],
+[[ ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿ ]],
+[[ ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ]],
+[[ ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟ ]],
+[[ ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠ ]],
+[[ ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙ ]],
+[[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
+*/
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <vector>
+using namespace std;
+
+vector<int> primes;
+void prime(int n) {
+        vector<bool> vis(n + 1);
+        for (int i = 2; i <= n; ++i) {
+                if (!vis[i]) {
+                        primes.push_back(i);
+                }
+                for (int j : primes) {
+                        if (i * j > n) {
+                                break;
+                        }
+                        vis[i * j] = true;
+                        if (i % j == 0) {
+                                break;
+                        }
+                }
+        }
+}
+
+signed main() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        prime(100);
+
+        int n, q;
+        cin >> n >> q;
+        vector<int> dp(n + 1);
+        map<int, int> num;
+        for (int i = 1, x; i <= n; ++i) {
+                cin >> x;
+                dp[i] = dp[i - 1];
+
+                int temp = 0;
+                for (int j = 0; x && j < (int)primes.size(); ++j) {
+                        while (x % primes[j] == 0) {
+                                temp ^= 1 << j;
+                                x /= primes[j];
+                        }
+                }
+                x = temp;
+
+                if (x) {
+                        dp[i] = max(dp[i], num[x]);
+                } else {
+                        dp[i] = i;
+                }
+
+                num[x] = i;
+
+                auto it = num.lower_bound(dp[i]);
+
+                map<int, int> t;
+                for (; it != num.end(); it = next(it)) {
+                        t[it->first] = it->second;
+                        t[it->first ^ x] = max(t[it->first ^ x], it->second);
+                }
+                num = t;
+        }
+
+        while (q--) {
+                int l, r;
+                cin >> l >> r;
+                cout << (dp[r] < l ? "No\n" : "Yes\n");
+        }
+
+        return 0;
+}
