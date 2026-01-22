c55b541196721171f7353db36bd52f087e49bc1f
Kicamon <kicamonice@gmail.com>
2026-01-22 19:30:44 +0800

    2026.01.22 19:30:44

---
+++ nc/周赛/127/b.cc
@@ -0,0 +1,76 @@
+/*
+[[ ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷ ]],
+[[ ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇ ]],
+[[ ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽ ]],
+[[ ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕ ]],
+[[ ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕ ]],
+[[ ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕ ]],
+[[ ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄ ]],
+[[ ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕ ]],
+[[ ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿ ]],
+[[ ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ]],
+[[ ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟ ]],
+[[ ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠ ]],
+[[ ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙ ]],
+[[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
+*/
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <array>
+using namespace std;
+
+bool check(int a, int b, int c, int d) {
+        array<int, 4> t = { a, b, c, d };
+        sort(t.begin(), t.end());
+        for (int i = 1; i < 4; ++i) {
+                if (t[i] != t[i - 1] + 1) {
+                        return false;
+                }
+        }
+        return true;
+}
+
+void solve() {
+        vector<vector<int>> a(5, vector<int>(5));
+        for (int i = 1; i <= 4; ++i) {
+                for (int j = 1; j <= 4; ++j) {
+                        cin >> a[i][j];
+                }
+                if (!check(a[i][1], a[i][2], a[i][3], a[i][4])) {
+                        cout << "NO\n";
+                        return;
+                }
+        }
+
+        for (int i = 1; i <= 4; ++i) {
+                if (!check(a[1][i], a[2][i], a[3][i], a[4][i])) {
+                        cout << "NO\n";
+                        return;
+                }
+        }
+
+        for (int i = 2; i <= 4; i += 2) {
+                for (int j = 2; j <= 4; j += 2) {
+                        if (!check(a[i - 1][j - 1], a[i - 1][j], a[i][j - 1], a[i][j])) {
+                                cout << "NO\n";
+                                return;
+                        }
+                }
+        }
+
+        cout << "YES\n";
+}
+
+signed main() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        int t;
+        cin >> t;
+        while (t--) {
+                solve();
+        }
+
+        return 0;
+}
---
+++ nc/周赛/127/e.cc
@@ -0,0 +1,95 @@
+/*
+[[ ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷ ]],
+[[ ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇ ]],
+[[ ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽ ]],
+[[ ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕ ]],
+[[ ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕ ]],
+[[ ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕ ]],
+[[ ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄ ]],
+[[ ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕ ]],
+[[ ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿ ]],
+[[ ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ]],
+[[ ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟ ]],
+[[ ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠ ]],
+[[ ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙ ]],
+[[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+using ll = long long;
+const int mod = 998244353;
+
+class C {
+    private:
+        vector<int> fct, ifct;
+
+    public:
+        C(int n) {
+                fct.resize(n);
+                ifct.resize(n);
+                fct[0] = ifct[0] = 1;
+                for (int i = 1; i < n; ++i) {
+                        fct[i] = (ll)fct[i - 1] * i % mod;
+                        ifct[i] = (ll)ifct[i - 1] * qmi(i, mod - 2) % mod;
+                }
+        }
+
+        int qmi(int a, int b) {
+                int res = 1;
+                while (b) {
+                        if (b & 1) {
+                                res = (ll)res * a % mod;
+                        }
+                        b >>= 1;
+                        a = (ll)a * a % mod;
+                }
+                return res;
+        }
+
+        int get(int a, int b) {
+                return (ll)fct[a] * ifct[b] % mod * ifct[a - b] % mod;
+        }
+};
+
+void solve() {
+        string s;
+        cin >> s;
+        int n = s.size();
+        if (n == 1) {
+                cout << 0 << '\n';
+                return;
+        }
+        s = '*' + s + '*';
+        int flag = 0, num = 0;
+        ll ans = 0, temp = 1;
+        for (int i = 1; i <= n; ++i) {
+                if (s[i] == s[i - 1]) {
+                        flag++;
+                }
+                if (s[i] != '?') {
+                        continue;
+                }
+                if (s[i - 1] == s[i + 1]) {
+                        temp = temp * 2 % mod;
+                } else {
+                        num++;
+                }
+        }
+        C c(num + 1);
+        int t = flag % 2;
+        cout << ans << '\n';
+}
+
+signed main() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        int t;
+        cin >> t;
+        while (t--) {
+                solve();
+        }
+
+        return 0;
+}
