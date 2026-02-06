bacc405d25cc082592dd5ba80aa303d616d5da7f
Kicamon <kicamonice@gmail.com>
2026-02-06 15:59:57 +0800

    2026.02.06 15:59:57

---
+++ nc/周赛/129/e.cc
@@ -0,0 +1,71 @@
+/*
+[[ ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷ ]],
+[[ ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇ ]],
+[[ ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽ ]],
+[[ ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕ ]],
+[[ ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕ ]],
+[[ ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕ ]],
+[[ ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄ ]],
+[[ ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕ ]],
+[[ ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿ ]],
+[[ ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ]],
+[[ ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟ ]],
+[[ ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠ ]],
+[[ ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙ ]],
+[[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+const int mod = 1e9 + 7;
+using ll = long long;
+
+class C {
+    private:
+        vector<int> fct, infct;
+
+    public:
+        C(int n) {
+                fct.resize(n);
+                infct.resize(n);
+                fct[0] = infct[0] = 1;
+                for (int i = 1; i < n; ++i) {
+                        fct[i] = (ll)fct[i - 1] * i % mod;
+                        infct[i] = (ll)infct[i - 1] * qmi(i, mod - 2, mod) % mod;
+                }
+        }
+
+        ll qmi(int a, int b, int p) {
+                ll res = 1;
+                while (b) {
+                        if (b & 1) {
+                                res = (ll)res * a % p;
+                        }
+                        b >>= 1;
+                        a = (ll)a * a % p;
+                }
+                return res;
+        }
+
+        int get(int a, int b) {
+                return (ll)fct[a] * infct[b] % mod * infct[a - b] % mod;
+        }
+};
+
+signed main() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        int n;
+        cin >> n;
+        ll tt = 1;
+        for (int i = 1; i < n; ++i) {
+                tt = tt * 2 % mod;
+        }
+        C c(n + 1);
+        for (int i = 1; i <= n; ++i) {
+                cout << tt * c.get(n, i) % mod << " \n"[i == n];
+        }
+
+        return 0;
+}
