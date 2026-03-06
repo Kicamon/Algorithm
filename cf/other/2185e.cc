df5696361e9f9902bd389145d7d0ebea755f3997
Kicamon <kicamonice@gmail.com>
2026-03-06 15:57:31 +0800

    2026.03.06 15:57:31

--- cf/other/2185e.cc
+++ cf/other/2185e.cc
@@ -15,6 +15,7 @@
 [[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
 */
 #include <algorithm>
+#include <functional>
 #include <iostream>
 #include <set>
 #include <vector>
@@ -26,7 +27,7 @@ void solve() {
         int n, m, k;
         cin >> n >> m >> k;
         vector<int> a(n), b(m);
-        vector<array<int, 2>> l(n), r(n);
+        vector<array<int, 2>> ldis(n), rdis(n);
         for (int i = 0; i < n; ++i) {
                 cin >> a[i];
         }
@@ -36,35 +37,52 @@ void solve() {
         sort(b.begin(), b.end());
         for (int i = 0; i < n; ++i) {
                 int idx = upper_bound(b.begin(), b.end(), a[i]) - b.begin();
-                l[i][0] = idx == 0 ? inf : a[i] - b[idx - 1];
-                r[i][0] = idx == m ? inf : b[idx] - a[i];
-                l[i][1] = r[i][1] = i;
+                ldis[i][0] = !idx ? inf : a[i] - b[idx - 1];
+                rdis[i][0] = idx == m ? inf : b[idx] - a[i];
+                ldis[i][1] = rdis[i][1] = i;
         }
-        sort(l.begin(), l.end());
-        sort(r.begin(), r.end());
-
-        char ch;
+        sort(ldis.begin(), ldis.end());
+        sort(rdis.begin(), rdis.end());
+        int l = 0, r = 0, L = 0, R = 0;
         set<int> st;
-        int ldis = 0, rdis = 0, L = 0, R = 0;
+
+        function<int(vector<array<int, 2>> &, int)> bs = [&](vector<array<int, 2>> &a, int x) {
+                int l = 0, r = a.size();
+
+                while (l < r) {
+                        int mid = (l + r) >> 1;
+                        if (a[mid][0] <= x) {
+                                l = mid + 1;
+                        } else {
+                                r = mid;
+                        }
+                }
+
+                return l;
+        };
+
         while (k--) {
+                char ch;
                 cin >> ch;
                 if (ch == 'L') {
-                        ldis++;
-                        rdis--;
+                        l++;
+                        r--;
                 } else {
-                        ldis--;
-                        rdis++;
+                        l--;
+                        r++;
                 }
-                int t = upper_bound(l.begin(), l.end(), array<int, 2>{ ldis, 0 }) - l.begin();
-                for (int i = L; i < t; ++i) {
-                        st.insert(l[i][1]);
+
+                int idx = bs(ldis, l);
+                for (int i = L; i < idx; ++i) {
+                        st.insert(ldis[i][1]);
                 }
-                L = max(L, t);
-                t = upper_bound(r.begin(), r.end(), array<int, 2>{ rdis, 0 }) - r.begin();
-                for (int i = R; i < t; ++i) {
-                        st.insert(r[i][1]);
+                L = max(L, idx);
+                idx = bs(rdis, r);
+                for (int i = R; i < idx; ++i) {
+                        st.insert(rdis[i][1]);
                 }
-                R = max(R, t);
+                R = max(R, idx);
+
                 cout << n - st.size() << ' ';
         }
         cout << '\n';
