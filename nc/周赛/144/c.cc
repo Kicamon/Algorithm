0db1d215b0fdb8614cc1323de63c7c3999d5b1fc
Kicamon <kicamonice@gmail.com>
2026-05-20 09:42:42 +0800

    2026.05.20 09:42:42

---
+++ nc/周赛/144/c.cc
@@ -0,0 +1,83 @@
+/*
+[[ ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷ ]],
+[[ ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇ ]],
+[[ ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽ ]],
+[[ ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕ ]],
+[[ ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕ ]],
+[[ ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕ ]],
+[[ ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄ ]],
+[[ ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕ ]],
+[[ ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿ ]],
+[[ ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ]],
+[[ ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟ ]],
+[[ ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠ ]],
+[[ ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙ ]],
+[[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> min_num(int n) {
+        vector<int> res(n);
+        res.back() = 1;
+        return res;
+}
+
+vector<int> max_num(int n) {
+        vector<int> res(n, 9);
+        return res;
+}
+
+vector<int> sub(vector<int> a, vector<int> b) {
+        vector<int> res = a;
+        int t = 0;
+        for (int i = 0; i < (int)a.size(); ++i) {
+                a[i] -= t;
+                t = 0;
+                if (i < (int)b.size()) {
+                        while (a[i] < b[i]) {
+                                t++;
+                                a[i] += 10;
+                        }
+                        res[i] = a[i] - b[i];
+                } else {
+                        while (a[i] < 0) {
+                                t++;
+                                a[i] += 10;
+                        }
+                        res[i] = a[i];
+                }
+        }
+        while (!res.back()) {
+                res.pop_back();
+        }
+        return res;
+}
+
+signed main() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        int n, m;
+        cin >> n >> m;
+        if (n < m) {
+                swap(n, m);
+        }
+        vector<int> t;
+        if (n == m) {
+                cout << "0 ";
+        } else {
+                t = sub(min_num(n), max_num(m));
+                for (int i = t.size() - 1; ~i; --i) {
+                        cout << t[i];
+                }
+                cout << " ";
+        }
+        t = sub(max_num(n), min_num(m));
+        for (int i = t.size() - 1; ~i; --i) {
+                cout << t[i];
+        }
+
+        return 0;
+}
