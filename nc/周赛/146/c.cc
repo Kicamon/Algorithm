fe8af1393deca1294330247e3145c2d79816c723
Kicamon <kicamonice@gmail.com>
2026-06-05 16:01:35 +0800

    2026.06.05 16:01:35

---
+++ nc/周赛/146/c.cc
@@ -0,0 +1,71 @@
+/*
+[[ ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷ ]],
+[[ ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇ ]],
+[[ ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽ ]],
+[[ ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕ ]],
+[[ ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕ ]],
+[[ ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕ ]],
+[[ ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄ ]],
+[[ ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕ ]],
+[[ ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿ ]],
+[[ ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ]],
+[[ ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟ ]],
+[[ ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠ ]],
+[[ ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙ ]],
+[[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
+*/
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+using ll = long long;
+
+int main() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        int n;
+        cin >> n;
+        map<int, vector<int>> mx, my;
+        for (int i = 0, x, y; i < n; ++i) {
+                cin >> x >> y;
+                mx[x].push_back(y);
+                my[y].push_back(x);
+        }
+
+        ll ans = 0;
+        for (auto [x, vt] : mx) {
+                for (int i = 0; i < (int)vt.size(); ++i) {
+                        for (int j = i + 1; j < (int)vt.size(); ++j) {
+                                int y = vt[i] + vt[j];
+                                if (y % 2) {
+                                        continue;
+                                }
+                                y /= 2;
+                                ans += my[y].size();
+                                if (find(my[y].begin(), my[y].end(), x) != my[y].end()) {
+                                        ans--;
+                                }
+                        }
+                }
+        }
+        for (auto [y, vt] : my) {
+                for (int i = 0; i < (int)vt.size(); ++i) {
+                        for (int j = i + 1; j < (int)vt.size(); ++j) {
+                                int x = vt[i] + vt[j];
+                                if (x % 2) {
+                                        continue;
+                                }
+                                x /= 2;
+                                ans += mx[x].size();
+                                if (find(mx[x].begin(), mx[x].end(), y) != mx[x].end()) {
+                                        ans--;
+                                }
+                        }
+                }
+        }
+        cout << ans;
+
+        return 0;
+}
