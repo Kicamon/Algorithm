7009ba9182b8878bf883cdd3b3c62ff612ae60b1
Kicamon <kicamonice@gmail.com>
2026-08-06 21:02:19 +0800

    2026.08.06 21:02:19

---
+++ cf/2248/b.cc
@@ -0,0 +1,71 @@
+/*
+[[ ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷ ]],
+[[ ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇ ]],
+[[ ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽ ]],
+[[ ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕ ]],
+[[ ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕ ]],
+[[ ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕ ]],
+[[ ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄ ]],
+[[ ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕ ]],
+[[ ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿ ]],
+[[ ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ]],
+[[ ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟ ]],
+[[ ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠ ]],
+[[ ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙ ]],
+[[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
+*/
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve() {
+        int n, m;
+        cin >> n >> m;
+        vector<int> a(n + 1), b(m + 1), from(n + 1), to(n + 1);
+        for (int i = 1; i <= n; ++i) {
+                cin >> a[i];
+        }
+        for (int i = 1; i <= m; ++i) {
+                cin >> b[i];
+        }
+        if (n < m * 2) {
+                cout << "NO\n";
+                return;
+        }
+        sort(a.begin() + 1, a.end());
+        sort(b.begin() + 1, b.end());
+        for (int i = 1; i <= m; ++i) {
+                int idx = lower_bound(a.begin(), a.end(), b[i]) - a.begin();
+                from[1]++, from[idx]--;
+                to[idx]--, to[n]++;
+        }
+        for (int i = 1; i <= n; ++i) {
+                from[i] += from[i - 1];
+                if (from[i] > i) {
+                        cout << "NO\n";
+                        return;
+                }
+        }
+        for (int i = n - 1; i >= 1; --i) {
+                to[i] += to[i + 1];
+                if (to[i] > n - i) {
+                        cout << "NO\n";
+                        return;
+                }
+        }
+        cout << "YES\n";
+}
+
+int main() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        int t;
+        cin >> t;
+        while (t--) {
+                solve();
+        }
+
+        return 0;
+}
